32c320854c7f186bafc0a2451e01a6910855d5ed
vamshikrishnaanandesi <vamshikrishnaanandesi@gmail.com>
2017-10-01 00:04:33 +0530

    flsk

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+#@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    return render_template('menu.html', restaurant = restaurant, items = items)
 
 if __name__ == '__main__':
     app.debug = True
